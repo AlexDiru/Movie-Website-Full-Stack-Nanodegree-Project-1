2a5cbba8167344454fb28c71b65ef5ed5cc724c1
Alex Spedding <alexspedding_oni@hotmail.com>
2017-11-11 15:36:54 +0000

    Somehow fresh tomatoes went missing; I hate command line Git

--- media.py
+++ media.py
@@ -6,7 +6,3 @@ class Movie():
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_url
-        self.rating
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
