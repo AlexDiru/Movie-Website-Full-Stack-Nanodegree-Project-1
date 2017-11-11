6eae2a358a6eba658dee97367a92009c348f9a56
Alex Spedding <alexspedding_oni@hotmail.com>
2017-11-11 15:29:50 +0000

    First Changes

---
+++ entertainment_centre.py
@@ -0,0 +1,10 @@
+import media
+import fresh_tomatoes
+
+crows_zero = media.Movie("Crows Zero", "Fighting in Suzuran", "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Crowszero.jpg/220px-Crowszero.jpg", "https://www.youtube.com/watch?v=6H3CMjsX2CE")
+crows_zero_2 = media.Movie("Crows Zero II", "Suzuran vs Housen", "http://blueprintreview.co.uk/wp-content/uploads/2012/07/Crows-Zero-2-movie-poster.jpg", "https://www.youtube.com/watch?v=hTPZW28r_K0")
+crows_explode = media.Movie("Crows Explode", "Genji's graduated", "http://www.martialartsmoviejunkie.com/wp-content/uploads/2014/01/Crows-Explode-Poster.jpg", "https://www.youtube.com/watch?v=fRMxLsNn-ZY")
+
+movies = [ crows_zero, crows_zero_2, crows_explode ]
+
+fresh_tomatoes.open_movies_page(movies)
