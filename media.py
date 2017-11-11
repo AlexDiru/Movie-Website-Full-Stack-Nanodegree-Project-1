557c5122e82d4ac192f1829047f54bb295c938a9
Alex Spedding <alexspedding_oni@hotmail.com>
2017-11-11 15:45:20 +0000

    Added error checking in the Movie constructor

--- media.py
+++ media.py
@@ -2,6 +2,16 @@ import webbrowser
 
 class Movie():
     def __init__(self, title, storyline, poster_image_url, trailer_url):
+        #Error checking
+        if title == "":
+            raise ValueError("Movie title is empty")
+        if storyline == "":
+            raise ValueError("Storyline is empty")
+        if poster_image_url == "":
+            raise ValueError("Poster Image URL is empty")
+        if trailer_url == "":
+            raise ValueError("Trailer URL is empty")
+        
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
